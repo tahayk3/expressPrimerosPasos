56aba64cbb1ccce93c6573a70b10b90f85ab78db
tahayk3 <tahayk3@gmail.com>
2024-02-05 12:17:55 -0600

    Creando proyecto, creando archivo que simula una base de datos, instando express

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+//Utilizamos este archivo para simular datos de una base de datos 
+const {infoCursos} =  require("./cursos.js");
+
+//Creamos un proyecto con express, por medio de la funcion express()
+const app = express();
+
+
+
