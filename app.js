91f52a8281cac5b63c83811a3932ee1732305b65
tahayk3 <tahayk3@gmail.com>
2024-02-05 12:51:20 -0600

    Manejando primera solicitud get / home

--- app.js
+++ app.js
@@ -5,5 +5,22 @@ const {infoCursos} =  require("./cursos.js");
 //Creamos un proyecto con express, por medio de la funcion express
 const app = express();
 
+/*Manejando rutas
+app: utilizar express
+get: tipo de solicitud
+"/". la ruta o path
+y de ultima una funcion flecha para realizar cierta accion
+*/
+app.get("/",(req,res)=>{
+    res.send("Primer servidor con Express");
+});
+
+/*process.env.PORT se utiliza para que en el despliegue, se utilice el puerto 
+asignado por algun servcio en la nube o hosting*/
+const PUERTO = process.env.PORT || 3000;
+app.listen(PUERTO,()=>{
+    console.log("Servidor escuchando, localhost:"+ PUERTO);
+});
+
 
 
